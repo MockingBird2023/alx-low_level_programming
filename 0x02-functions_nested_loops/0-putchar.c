3a85700d6cb1ea9513cb6dc0eab6fc162114aeb0
MockingBird2023 <sulleyadizatu@gmail.com>
2023-03-21 20:52:38 -0700

    error

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
-/**
- * main - Entry point  
- * Return: Always 0
- */
+	/**
+	 * main - Entry point
+	 * Description: putcharcode
+	 * @parameter: int 
+	 * Return: Always 0
+	 */
 
-int main(void)
-{	
+	int main(void)
+	{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -17,4 +19,4 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-}
+	}
