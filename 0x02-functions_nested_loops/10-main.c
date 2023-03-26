5938e2d77cf978d4772c7f60998130aa308e6313
MockingBird2023 <sulleyadizatu@gmail.com>
2023-03-26 09:42:46 -0700

    lip

--- 0x02-functions_nested_loops/10-main.c
+++ 0x02-functions_nested_loops/10-main.c
@@ -6,11 +6,12 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    int n;
+	int n;
 
-    n = add(89, 9);
-    printf("%d\n", n);
-    return (0);
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
 }
