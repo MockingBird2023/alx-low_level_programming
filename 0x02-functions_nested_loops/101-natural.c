0dea0ea39344a402b022172e86c2c5a2ddd67e6e
MockingBird2023 <sulleyadizatu@gmail.com>
2023-03-26 10:30:36 -0700

    try

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,31 +1,23 @@
 #include <stdio.h>
 
-	/**
-	 * main - finds and prints the sum of the even-valued terms
-	 * followed by a new line
-	 * Return: Always 0 (Success)
-	 */
-	int main(void)
+/**
+ * main - Lists all the natural numbers below 1024 (excluded)
+ * that are multiples of 3 or 5.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
 	{
-		int i;
-		unsigned long int j, k, next, sum;
-
-		j = 1;
-		k = 2;
-		sum = 0;
-
-		for (i = 1; i <= 33; ++i)
-		{
-			if (j < 4000000 && (j % 2) == 0)
-			{
-				sum = sum + j;
-			}
-			next = j + k;
-			j = k;
-			k = next;
-		}
-
-		printf("%lu\n", sum);
-
-		return (0);
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
 	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
