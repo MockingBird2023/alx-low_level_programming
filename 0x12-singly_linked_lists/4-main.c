e1a758490e090e08867d55c22d81a9fac2af5f8a
MockingBird2023 <sulleyadizatu@gmail.com>
2023-04-28 07:06:58 -0700

    fee

--- 0x12-singly_linked_lists/4-main.c
+++ 0x12-singly_linked_lists/4-main.c
@@ -5,6 +5,16 @@
  */
 int main(void)
 {
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
+	list_t *head;
+
+	head = NULL;
+	add_node_end(&head, "Bob");
+	add_node_end(&head, "&");
+	add_node_end(&head, "Kris");
+	add_node_end(&head, "love");
+	add_node_end(&head, "asm");
+	print_list(head);
+	free_list(head);
+	head = NULL;
+	return (0);
 }
